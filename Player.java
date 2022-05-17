0724c151f3dc81078eb9dd1381b25a35f11cb053
KD54321 <98127805+KD54321@users.noreply.github.com>
2022-05-17 12:52:51 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,7 +2,7 @@
  * The Player class creates a object of type player with a playerposition and a dicevalue
  * @param name a string for the player's name
  * @return the position of a player as an int, the value of the rolled dice as an int
- * @author Kevin Duong(40209877) and Paul Touma (40210678)
+
  *@version1.0
  */
 public class Player  {
